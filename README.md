97f1bd882e986d6fc21f4dc777a4a95310aa0c6f
manhph2211 <maxph22112000@gmail.com>
2021-06-29 22:13:46 +0700

    update db mmocr

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# Text-Detection
-This repo is about to implement different kinds of models to do the task of detecting text in the image :smile:
+# Text Detection :smile:
+=====
+
+- This repo is about to implement diffrent kinds of modern toolkits to detect texts in image.
+
+- Let's start with:
+
+```
+git clone https://github.com/manhph2211/ICDAR-2015.git 
+cd ICDAR-2015
+
+```
+
+## Datasets
+
+- This implement uses datasets from ICDAR-2015 competiton. You can download data from the [offical website](https://rrc.cvc.uab.es/?ch=4&com=downloads) (in the task 4.1). All zip files should be unzipped at `/ICDAR-2015` 
+
+## Train
+
+- Try:
+
+```
+bash bash.sh
+cd mmocr
+python3 ./tools/train.py ../trainer.py
+
+```
+
+
+
+
+
+
+
+
---
+++ bash.sh
@@ -0,0 +1,30 @@
+
+pip install mmcv-full==1.3.4
+
+# install mmdetection
+pip install mmdet==2.11.0
+
+# install mmocr
+git clone https://github.com/open-mmlab/mmocr.git
+cd mmocr
+pip install -r requirements.txt
+pip install -v -e .  
+mkdir checkpoints
+cd ..
+
+#-----------------------------------------------------------------------------
+cd data
+# For images
+mv ../ch4_training_images imgs/train
+mv ../ch4_test_images imgs/test
+# For annotations
+mv ../ch4_training_localization_transcription_gt annotations/train
+mv ../Challenge4_Test_Task1_GT annotations/test
+
+#-----------------------------------------------------------------------------
+
+wget https://download.openmmlab.com/mmocr/data/icdar2015/instances_training.json
+wget https://download.openmmlab.com/mmocr/data/icdar2015/instances_test.json
+wget -P ../mmocr/checkpoints https://download.openmmlab.com/mmocr/textdet/dbnet/dbnet_r18_fpnc_sbn_1200e_icdar2015_20210329-ba3ab597.pth
+
+cd ..
